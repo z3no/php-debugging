3c9f3ae8326beb1d11bb7692616ca61712d2be30
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-17 14:46:00 +0200

    solved exerc 4
    took some time, googled totally wrong, then started simple by looking at the foreach function on php.net and directly found it

--- expert.php
+++ expert.php
@@ -33,4 +33,22 @@ $str = "Debugged ! Also very fun";
 echo substr($str, 0, 10);
 
 
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
 
+// INFORMATION FOUND ON php.net
+// In order to be able to directly modify array elements within the loop precede $value with &. In that case the value will be assigned by reference.
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
+
+
+
+
+new_exercise(5);
