925f618bcd3ecacb5aa0a024b4d084444a1e2ece
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-17 11:48:55 +0200

    debugged exercises 1-3
    Lost some time with setting up xdebug, now finally time to debug muahaha

--- expert.php
+++ expert.php
@@ -1,6 +1,36 @@
 <?php
 declare(strict_types=1);
 
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+
+function new_exercise($x="1") {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+
+}
+
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = "Debugged ! Also very fun";
+echo substr($str, 0, 10);
 
 
 
