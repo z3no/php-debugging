aed3871fc6d9c442c50c0e271b0e0ae366d687b5
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-20 13:18:26 +0200

    tried another crack at 6
    Haven't managed to solve it yet

--- expert.php
+++ expert.php
@@ -29,8 +29,8 @@ new_exercise(3);
 // === Exercise 3 ===
 // This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
 
-$str = "Debugged ! Also very fun";
-echo substr($str, 0, 10);
+$str = '` "Debugged !" `  Also very fun" `';
+echo substr($str, 0, 16);
 
 
 new_exercise(4);
